6db8e280194a949cf6c01d0e92a77b43870aaa6d
PhilippOberbauer <OberbauerP190207@sus.htl-grieskirchen.at>
2022-10-08 18:07:40 +0200

    implemented class Predicates

---
+++ Hausuebung3/src/beispiel3/Predicates.java
@@ -0,0 +1,36 @@
+package beispiel3;
+
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+
+public class Predicates {
+    public Predicate<Integer> isEven()
+    {
+        final Predicate<Integer> isEven = i -> i % 2 == 0;
+        return isEven;
+    }
+
+    public IntPredicate isPositive()
+    {
+        final IntPredicate isPositive = i -> i > 0;
+        return isPositive;
+    }
+
+    public IntPredicate isZero()
+    {
+        final IntPredicate isZero = i -> i == 0;
+        return isZero;
+    }
+
+    public Predicate<String> isNull()
+    {
+        final Predicate<String> isNull = s -> s == null;
+        return isNull;
+    }
+
+    public Predicate<String> isShortWord()
+    {
+        final Predicate<String> isShortWord = s -> s.length() < 4;
+        return isShortWord;
+    }
+}
